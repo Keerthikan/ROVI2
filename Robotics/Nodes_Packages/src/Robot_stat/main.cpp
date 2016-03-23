aa7ec70cccbbfbb9cd6c2d89fa60283e809c3392
Krill14 <kristoffer_3@msn.com>
2016-03-23 15:00:21 +0100

    robot main cpp

--- Robotics/Nodes_Packages/src/Robot_stat/main.cpp
+++ Robotics/Nodes_Packages/src/Robot_stat/main.cpp
@@ -24,11 +24,18 @@ protected:
     ros::Subscriber sub;
 
 
+//    caros_control_msgs::RobotState newRobotState;
+//    ros::Publisher pub;
+
+//private:
+//    int count = 0;
+
 public:
 
     robot_state_node(ros::NodeHandle& nh):nh_(nh)
     {
         sub = nh.subscribe("/caros_universalrobot/caros_serial_device_service_interface/robot_state", 1, &robot_state_node::robotCb, this);
+//        pub = nh.advertise<caros_control_msgs::RobotState>("chatter", 1000);
     }
 
     void robotCb(const caros_control_msgs::RobotStateConstPtr& robotstate)
@@ -42,13 +49,27 @@ public:
 
     void robot_state()
     {
-        if ( robot_stateIn == NULL)
+        if ( robot_stateIn == NULL || robot_stateIn == robot_stateIn_temp)
         return;
 
         ROS_ERROR("process");
         robot_stateIn_temp = robot_stateIn;
         rw::math::Q newQ = caros::toRw(robot_stateIn_temp.get()->q);
         std::cout << newQ << std::endl;
+
+//        if(count == 0)
+//        {
+//            rw::math::Q newnewQ[6]{0, 0, 0, 0, 0, 0};
+//            newRobotState = robot_stateIn_temp;
+
+//            newRobotState.q = caros::toRos(newnewQ);
+//            newRobotState.header.stamp = ros::Time::now();
+
+//            newRobotState.is_colliding = caros::toRos(false);
+
+//            pub.publish(newRobotState);
+//            count++;
+//        }
         //        std::cout << robot_stateIn_temp.get()->header.stamp.now() << std::endl;
         //        std::cout <<  robot_stateIn_temp->q << std::endl;
     }
