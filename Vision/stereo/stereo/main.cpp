49f0fe82b947119cdb495bbfa180be7bd8fd06ee
Keerthikan <kerat12@student.sdu.dk>
2016-03-24 11:06:56 +0100

    thrash

--- Vision/stereo/stereo/main.cpp
+++ Vision/stereo/stereo/main.cpp
@@ -13,36 +13,70 @@
 using namespace std;
 using namespace cv;
 
-//  cam0=   [3962.004 0 1146.717;
-//           0 3962.004 975.476;
-//           0 0 1]
+// Types
+typedef cv::Mat_<cv::Vec3f> ImgT; // 3-channel image
 
-//  cam1=[3962.004 0 1254.628;
-//        0 3962.004 975.476;
-//        0 0 1]
+//slider values
+const int hSlider = 180;
+int hSliderMin;
+int hSliderMax;
 
-//  doffs=107.911 -  x-difference of principal points, doffs = cx1 - cx0
+const int sSlider = 255;
+int sSliderMin;
+int sSliderMax;
 
-//  baseline=237.604  - camera baseline in mm
+const int vSlider = 255;
+int vSliderMin;
+int vSliderMax;
 
-//  width=3000
-//  height=1920 -   image size
+double hue;
+double beta;
 
-//  ndisp=250 - a conservative bound on the number of disparity levels;
-              //the stereo algorithm MAY utilize this bound and search from d = 0 .. ndisp-1
-
-//  isint=0 -   whether the GT disparites only have integer precision (true for the older datasets;
-                //in this case submitted floating-point disparities are rounded to ints before evaluating)
-
-//  vmin=12     a tight bound on minimum and maximum disparities, used for color visualization;
-//  vmax=213    the stereo algorithm MAY NOT utilize this information
+cv::Mat dst;
+cv::Mat imghsv;
 
 
-//  dyavg=0     average and maximum absolute y-disparities, providing an indication of
-//  dymax=0     the calibration error present in the imperfect datasets.
+void separateChannels (cv::Mat img) {
+    //initialize a matrix for each channel in img
+    cv::Mat ch1(img.rows, img.cols, CV_8UC1);
+    cv::Mat ch2(img.rows, img.cols, CV_8UC1);
+    cv::Mat ch3(img.rows, img.cols, CV_8UC1);
+    
+    //initialize an int array of size 2
+    int from_to[2];
+    
+    //copy channel 2 of img to channel 0 of ch1
+    from_to[0] = 2;
+    from_to[1] = 0;
+    cv::mixChannels( &img, 1, &ch1, 1, from_to, 1);
+    //cv::imshow("Value", ch1);
+    
+    //copy channel 1 of img to channel 0 of ch2
+    from_to[0] = 1;
+    from_to[1] = 0;
+    cv::mixChannels( &img, 1, &ch2, 1, from_to, 1);
+    //cv::imshow("Saturation", ch2);
+    
+    //copy channel 0 of img to channel 0 of ch3
+    from_to[0] = 0;
+    from_to[1] = 0;
+    cv::mixChannels( &img, 1, &ch3, 1, from_to, 1);
+    //cv::imshow("Hue", ch3);
+    
+    //cv::waitKey(0);
+}
+
+void on_trackbar( int, void* )
+{
+    cv::inRange(imghsv, cv::Scalar(hSliderMin, sSliderMin, vSliderMin), cv::Scalar(hSliderMax, sSliderMax, vSliderMax), dst);
+    cout << "Hmin: " << hSliderMin << " Hmax: " << hSliderMax << endl;
+    cout << "Smin: " << sSliderMin << " Smax: " << sSliderMax << endl;
+    cout << "Vmin: " << vSliderMin << " Vmax: " << vSliderMax << endl;
+    imshow( "Colour Segmentation", dst );
+}
 
 
-// Z = baseline * f / (d + doffs)
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
@@ -50,23 +84,59 @@ int main(int argc, const char * argv[]) {
     std::cout << "Open CV Version " << CV_VERSION << std::endl;
     
     
-    Mat g1, g2;
-    Mat disp, disp8;
-    
-    Mat LeftImage = imread("/Users/keerthikanratnarajah/Dropbox/Eksamen/ROVI2/Project/Vision/Classroom1-perfect/im0.png");
-    Mat RightImage = imread("/Users/keerthikanratnarajah/Dropbox/Eksamen/ROVI2/Project/Vision/Classroom1-perfect/im1.png");
-    
-    cvtColor(LeftImage, g1, CV_BGR2GRAY);
-    cvtColor(RightImage, g2, CV_BGR2GRAY);
-    
+    cv::Mat img = cv::imread("/Users/keerthikanratnarajah/Dropbox/Eksamen/ROVI2/Project/Vision/stereo/images.png");
 
     
-    //namedWindow("left",CV_WINDOW_KEEPRATIO);
-    //namedWindow("right",CV_WINDOW_KEEPRATIO);
+    //Part 1
+    cv::Mat imgBGR[3];
+    cv::split(img,imgBGR);
+    //cv::imshow("Blue",imgBGR[0]);
+    //cv::imshow("Green",imgBGR[1]);
+    //cv::imshow("Red",imgBGR[2]);
+    //cv::waitKey(0);
+    //cv::destroyWindow("Red");
     
-    //imshow("left", LeftImage);
-    //imshow("right", RightImage);
+    //Part 2
+    cv::cvtColor(img, imghsv, CV_BGR2HSV);
+    separateChannels(imghsv); //alternatively use cv::split() as above
     
-    waitKey();
+    //Part 3
+    hSliderMin = 0;
+    hSliderMax = 180;
+    sSliderMin = 0;
+    sSliderMax = 255;
+    vSliderMin = 0;
+    vSliderMax = 255;
+    
+    namedWindow("Colour Segmentation", cv::WINDOW_AUTOSIZE);
+    
+    cv::createTrackbar( "Hue min", "Colour Segmentation", &hSliderMin, hSlider, on_trackbar );
+    cv::createTrackbar( "Hue max", "Colour Segmentation", &hSliderMax, hSlider, on_trackbar );
+    cv::createTrackbar( "S min", "Colour Segmentation", &sSliderMin, sSlider, on_trackbar );
+    cv::createTrackbar( "S max", "Colour Segmentation", &sSliderMax, sSlider, on_trackbar );
+    cv::createTrackbar( "V min", "Colour Segmentation", &vSliderMin, vSlider, on_trackbar );
+    cv::createTrackbar( "V max", "Colour Segmentation", &vSliderMax, vSlider, on_trackbar );
+    cv::waitKey(0);
+    
+    //Part 4
+    
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    
+    findContours( imghsv, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+    
+    Mat drawing = Mat::zeros( imghsv.size(), CV_8UC3 );
+    for( int i = 0; i< contours.size(); i++ )
+    {
+        Scalar color = Scalar( 255,0,0 );
+        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+    }
+    
+    /// Show in a window
+    namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+    imshow( "Contours", drawing );
+    cv::waitKey(0);
+
+    
     return 0;
 }
--- Vision/stereo2/stereo2/main.cpp
+++ Vision/stereo2/stereo2/main.cpp
@@ -6,10 +6,121 @@
 //  Copyright (c) 2016 Keerthikan Ratnarajah. All rights reserved.
 //
 
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/utility.hpp>
 #include <iostream>
+#include <stdio.h>
+
+
+using namespace cv;
+
+static void print_help()
+{
+    printf("\nDemo stereo matching converting L and R images into disparity and point clouds\n");
+    printf("\nUsage: stereo_match <left_image> <right_image> [--algorithm=bm|sgbm|hh|sgbm3way] [--blocksize=<block_size>]\n"
+           "[--max-disparity=<max_disparity>] [--scale=scale_factor>] [-i=<intrinsic_filename>] [-e=<extrinsic_filename>]\n"
+           "[--no-display] [-o=<disparity_image>] [-p=<point_cloud_file>]\n");
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    std::string img1_filename = "/Users/keerthikanratnarajah/Dropbox/Eksamen/ROVI2/Project/Vision/stereo/left.png";
+    std::string img2_filename = "/Users/keerthikanratnarajah/Dropbox/Eksamen/ROVI2/Project/Vision/stereo/right.png";
+    //std::string intrinsic_filename = "";
+    //std::string extrinsic_filename = "";
+    //std::string disparity_filename = "";
+    //std::string point_cloud_filename = "";
+    
+    enum { STEREO_BM=0, STEREO_SGBM=1, STEREO_HH=2, STEREO_VAR=3, STEREO_3WAY=4 };
+    int alg = STEREO_SGBM;
+    int SADWindowSize, numberOfDisparities;
+    bool no_display;
+    float scale;
+
+    
+    Ptr<StereoBM> bm = StereoBM::create(16,9);
+    Ptr<StereoSGBM> sgbm = StereoSGBM::create(0,16,3);
+    cv::CommandLineParser parser(argc, argv,
+                                 "{@arg1||}{@arg2||}{help h||}{algorithm||}{max-disparity|0|}{blocksize|0|}{no-display||}{scale|1|}{i||}{e||}{o||}{p||}");
+    if(parser.has("help"))
+    {
+        print_help();
+        return 0;
+    }
+    img1_filename = parser.get<std::string>(0);
+    img2_filename = parser.get<std::string>(1);
+    if (parser.has("algorithm"))
+    {
+        std::string _alg = parser.get<std::string>("algorithm");
+        alg = _alg == "bm" ? STEREO_BM :
+        _alg == "sgbm" ? STEREO_SGBM :
+        _alg == "hh" ? STEREO_HH :
+        _alg == "var" ? STEREO_VAR :
+        _alg == "sgbm3way" ? STEREO_3WAY : -1;
+    }
+    numberOfDisparities = parser.get<int>("max-disparity");
+    SADWindowSize = parser.get<int>("blocksize");
+    scale = parser.get<float>("scale");
+    no_display = parser.has("no-display");
+    if( parser.has("i") )
+        intrinsic_filename = parser.get<std::string>("i");
+    if( parser.has("e") )
+        extrinsic_filename = parser.get<std::string>("e");
+    if( parser.has("o") )
+        disparity_filename = parser.get<std::string>("o");
+    if( parser.has("p") )
+        point_cloud_filename = parser.get<std::string>("p");
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 1;
+    }
+    if( alg < 0 )
+    {
+        printf("Command-line parameter error: Unknown stereo algorithm\n\n");
+        print_help();
+        return -1;
+    }
+    if ( numberOfDisparities < 1 || numberOfDisparities % 16 != 0 )
+    {
+        printf("Command-line parameter error: The max disparity (--maxdisparity=<...>) must be a positive integer divisible by 16\n");
+        print_help();
+        return -1;
+    }
+    if (scale < 0)
+    {
+        printf("Command-line parameter error: The scale factor (--scale=<...>) must be a positive floating-point number\n");
+        return -1;
+    }
+    if (SADWindowSize < 1 || SADWindowSize % 2 != 1)
+    {
+        printf("Command-line parameter error: The block size (--blocksize=<...>) must be a positive odd number\n");
+        return -1;
+    }
+    if( img1_filename.empty() || img2_filename.empty() )
+    {
+        printf("Command-line parameter error: both left and right images must be specified\n");
+        return -1;
+    }
+    if( (!intrinsic_filename.empty()) ^ (!extrinsic_filename.empty()) )
+    {
+        printf("Command-line parameter error: either both intrinsic and extrinsic parameters must be specified, or none of them (when the stereo pair is already rectified)\n");
+        return -1;
+    }
+    
+    if( extrinsic_filename.empty() && !point_cloud_filename.empty() )
+    {
+        printf("Command-line parameter error: extrinsic and intrinsic parameters must be specified to compute the point cloud\n");
+        return -1;
+    }
+    
+    int color_mode = alg == STEREO_BM ? 0 : -1;
+    Mat img1 = imread(img1_filename, color_mode);
+    Mat img2 = imread(img2_filename, color_mode);
+    
+    
     return 0;
 }
