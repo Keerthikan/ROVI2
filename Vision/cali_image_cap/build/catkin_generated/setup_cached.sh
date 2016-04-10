4fd26965274f7a7b9cb957f3345e2cc2f5490641
Keerthikan <kerat12@student.sdu.dk>
2016-04-10 12:11:15 +0200

    local Kiddi

--- Vision/cali_image_cap/build/catkin_generated/setup_cached.sh
+++ Vision/cali_image_cap/build/catkin_generated/setup_cached.sh
@@ -7,6 +7,12 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/x86_64-linux-gnu:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/pkgconfig:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/x86_64-linux-gnu/pkgconfig:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/build"
+export PYTHONPATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/src:$ROS_PACKAGE_PATH"
--- Vision/cali_image_cap/devel/_setup_util.py
+++ Vision/cali_image_cap/devel/_setup_util.py
@@ -260,7 +260,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali_image_cap/devel;/opt/ros/indigo'.split(';')
+        CMAKE_PREFIX_PATH = '/home/keerthikan/Dropbox/Eksamen/ROVI2/Project/Vision/cali/devel;/opt/ros/indigo'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:
